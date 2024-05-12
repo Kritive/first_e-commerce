5605a50b6a616120c40f1aa9aa4f0f4fc2b53781
Kritive Bhandari <kritiveb@gmail.com>
2024-05-12 15:46:36 +0545

    Commit message

---
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import MainCrausel from '../../components/HomeCrausel/MainCrausel'
+
+const HomePage = () => {
+  return (
+    <div>
+       <MainCrausel/>
+       <div>
+         other section
+       </div>
+    </div>
+  )
+}
+
+export default HomePage;
